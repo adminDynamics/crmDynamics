a30370adc7d3b0a489b37dbf5d4f5ca87318dfa0
adminDynamics <sebastian.schmittendorf@dynamics-ia.com>
2025-06-20 15:48:54 -0300

    agregamos el front

--- whatsapp-crm/app/components/twilio-config.tsx
+++
@@ -1,9 +0,0 @@
-import { ConnectionStatus } from "./connection-status"
-
-interface TwilioConfigProps {
-  onConfigSaved: () => void
-}
-
-export function TwilioConfig({ onConfigSaved }: TwilioConfigProps) {
-  return <ConnectionStatus />
-}
